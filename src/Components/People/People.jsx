c4c49bb71372f0fc3b3b56c1efe13f4af721115e
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-16 17:28:15 +0200

    Finish People Search.

--- src/Components/People/People.jsx
+++ src/Components/People/People.jsx
@@ -33,7 +33,27 @@ export default function People() {
     getTrending()
   }, [])
 
+  async function search(e) {
+    const inputValue = e.target.value.trim(); // Trim to handle whitespace
+    if (inputValue === '') {
+      // Call trending API when input is empty
+      getTrending();
+    } else {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2NhMTY0YWIwYWI0YTY5ZTQ5NTk4Y2UzNjkxZWY4ZSIsInN1YiI6IjY0MzVlYjkwOWFjNTM1MDA5ZDM3Yzg3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yJl7Xv--ydm8fJn9K3DkR2Op7DE9FnwVFJa16eB1myU'
+        }
+      };
 
+      setIsLoading(true);
+      let { data } = await axios.get(`https://api.themoviedb.org/3/search/person?query=${inputValue}&include_adult=false&language=en-US&page=1`, options);
+      setPeoples(data.results.map((tvShow) => ({ ...tvShow, media_type: 'person' })));
+      console.log(data.results);
+      setIsLoading(false);
+    }
+  }
 
 
 
@@ -56,6 +76,10 @@ export default function People() {
 
             </ul>
           </nav>
+
+
+          <input onChange={search} type="text" className='form-control  mb-5 bg-dark text-white' placeholder='Search...' />
+
           {isLoading ? <Loading /> : (<div className="row">
             {peoples?.map((person) => <Item data={person} key={person.id} />)}
           </div>)}
